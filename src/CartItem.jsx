e923bfb27555d358010fa6ac1c4b4d3ba208b494
Brandon <bteng434@gmail.com>
2026-06-20 21:12:00 -0400

    third task

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -7,29 +7,54 @@ const CartItem = ({ onContinueShopping }) => {
   const cart = useSelector(state => state.cart.items);
   const dispatch = useDispatch();
 
+      const parseItemCostToInteger = (itemCost) => {
+
+        return parseInt(itemCost.replace('$', ''), 10);
+    };
+
+
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
+        let totalCost = 0;
+
+        cart.forEach((item) => {
+            const itemCost = parseItemCostToInteger(item.cost);
+            totalCost += itemCost * item.quantity;
+        });
+
+        return totalCost;
  
   };
 
   const handleContinueShopping = (e) => {
-   
+     onContinueShopping(e);
   };
 
+  const handleCheckoutShopping = (e) => {
+      alert('Functionality to be added for future reference');
 
+};
 
   const handleIncrement = (item) => {
+    dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
   };
 
   const handleDecrement = (item) => {
-   
+   dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
   };
 
   const handleRemove = (item) => {
+      dispatch(removeItem(item.name));
+
   };
 
   // Calculate total cost based on quantity for an item
   const calculateTotalCost = (item) => {
+           let totalCost = 0;
+        const itemCost = parseItemCostToInteger(item.cost);
+        totalCost = item.quantity * itemCost;
+
+        return totalCost;
   };
 
   return (
@@ -57,7 +82,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
